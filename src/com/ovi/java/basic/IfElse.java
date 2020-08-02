1a06813bdad9cfdcd1e3301eccf79e3772b8928c
Ovindi Bandara <ovindi95@gmail.com>
2020-08-02 22:25:00 +0530

    if else

--- src/com/ovi/java/basic/IfElse.java
+++ src/com/ovi/java/basic/IfElse.java
@@ -2,6 +2,20 @@ package com.ovi.java.basic;
 import java.util.Scanner;
 
 public class IfElse {
-    
-
-}
+    public static void main(String args[])
+    {
+        int n;
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter number");
+        n=s.nextInt();
+        if (n>10){
+            System.out.println("greater than 10");
+        }
+        else if (n==10){
+            System.out.println("Equal to 10");
+        }
+        else{
+            System.out.println("less than 10");
+        }
+    }
+    }
